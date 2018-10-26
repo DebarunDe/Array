e7d99846652576c7de73e5bc1bdfc1a9aef8f7ff
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2018-10-26 14:27:07 -0400

    Add files via upload

---
+++ src/ArrayMain.java
@@ -0,0 +1,18 @@
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+
+       Dice dice1 = new Dice();
+       Dice dice2 = new Dice();
+       
+       int[]values = new int[13];
+       
+       for(int i = 0; i < 1000; i++)
+       {
+    	   
+       }
+
+	}
+
+}
